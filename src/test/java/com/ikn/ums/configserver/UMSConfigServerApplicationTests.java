b2612333875a3f14a566c6685118ef7e260048e2
Mahesh Jarati <mahesh.jarati@ikcontech.com>
2023-08-28 20:01:45 +0530

    initial commit

---
+++ src/test/java/com/ikn/ums/configserver/UMSConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMSConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
